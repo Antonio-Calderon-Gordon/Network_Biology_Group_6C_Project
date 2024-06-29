2a1f4b9b941fb6a73eb64d460b639aab4c976898
Antonio-Calderon-Gordon <antonio.c.gordon@gmail.com>
2024-06-29 22:03:20 +0200

    Add files via upload

---
+++ Packages.R
@@ -0,0 +1,42 @@
+#This file contains all the packages that have been used to conduct our analysis in R.
+
+#Packages used for the exploratory data analysis:
+
+library(rstudioapi)
+library(EnhancedVolcano)
+library(readxl)
+
+#Packages used for the enrichment analysis and pathway analysis:
+
+library(clusterProfiler)
+library(rWikiPathways)
+library(RCy3)
+library(org.Hs.eg.db)
+library(enrichplot)
+library(msigdbr)
+library(dplyr)
+library(RColorBrewer) 
+
+#Packages used for the network analysis and drug analysis:
+
+library(RCy3)
+library(stringi)
+library(RCy3)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(rWikiPathways)
+
+#Complete list of all packages used:
+
+library(rstudioapi)
+library(EnhancedVolcano)
+library(readxl)
+library(RCy3)
+library(stringi)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(rWikiPathways)
+library(enrichplot)
+library(msigdbr)
+library(dplyr)
+library(RColorBrewer) 
